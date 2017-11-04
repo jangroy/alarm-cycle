ebb90d63d762e76b94c20d4e2c9ab0048ad907bc
jangroy <jangroy@hotmail.com>
2017-11-03 18:36:00 -0700

    functioning clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+
+
+
+class Clock extends React.Component {
+  constructor(props) { 
+    super(props) // base constructor
+    this.state = { date: new Date() } // initial state
+  }
+  // lifecycle hooks
+  componentDidMount() {
+    this.timerID = setInterval(
+     () => this.tick(),
+     1000 
+    )
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+  // when tick is called, setState to the new Date
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
+        <Clock />
+      <div>
+      I want to wake up at ... 
+      </div>
+      <div>
+      I'm going to sleep at ...
+      </div>
       </div>
     );
   }
